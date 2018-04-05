eca5a3c6f45fe9b8f24b1404295c4da48bea4341
mdeejay <mdjrussia@gmail.com>
2018-04-05 11:30:18 +0700

    vince fingerprint

--- lineage.mk
+++ lineage.mk
@@ -25,7 +25,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V8.5.4.0.NCFMIED release-keys"
+    PRIVATE_BUILD_DESC="vince-user 7.1.2 N2G47H V9.5.3.0.NEGMIFA release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/vince/vince:7.1.2/N2G47H/V9.5.3.0.NEGMIFA:user/release-keys"
