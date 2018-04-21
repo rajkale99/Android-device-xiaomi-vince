046101ac9af596e384c3e377c0fd5b3163ab1a19
mdeejay <mdjrussia@gmail.com>
2018-04-21 23:23:49 +0700

    fixed dt2w path, clean up prop. files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 DEVICE_PATH := device/xiaomi/vince
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input2/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/78b7000.i2c/i2c-3/3-0020/input/input2/wake_gesture"
 
 # Filesystem
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
