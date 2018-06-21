d363fd10cd95d719a4a7dfad96305e7c719f9ad2
Madkita <abubakaryagob@gmail.com>
2018-06-22 04:50:14 +0800

    Revert "add prebuild Modded Gcam"
    This reverts commit 50211dd490438d312071e1d63673f17b3b54d54a.

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    MGC \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
